ca81d9282f1fb78d5cd05e8cdf7496b7bfbffd82
Krish Bakshi <krishbakshi23@gmail.com>
2025-05-03 19:50:53 +0530

    added vercel analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ChatBox from './ChatBox';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="bg-gray-50 min-h-screen flex items-center justify-center">
       <ChatBox />
+      <Analytics />
     </div>
   );
 }
